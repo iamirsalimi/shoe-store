ed1d0dd7087670db63acda57cc16aa81a4717e5c
iamirsalimi <wonderguy2003@gmail.com>
2023-10-25 23:32:04 +0330

    header completed

---
+++ public/js/main.js
@@ -0,0 +1,40 @@
+let hamburger = document.getElementById('hamburger-menu')
+let menu = document.getElementById('menu')
+let closeModalBtn = document.getElementById('closeModalBtn')
+let navElems = document.querySelectorAll('nav ul')
+
+
+
+const showMenu = () => {
+    let menuClass = menu.className
+    menu.parentNode.className = menu.parentNode.className.replace('invisible' , 'visible')
+    menu.parentNode.className = menu.parentNode.className.replace('z-0' , 'z-20')
+    menu.className = menuClass.replace('translate-x-full' , 'translate-x-0')
+    menu.parentNode.className = menu.parentNode.className.replace('bg-black/0' , 'bg-black/50') 
+}
+
+const closeMenu =  () => {
+    let menuClass = menu.className
+    menu.className = menuClass.replace('translate-x-0' , 'translate-x-full')
+    menu.parentNode.className = menu.parentNode.className.replace('bg-black/50' , 'bg-black/0') 
+    setTimeout(() => {
+        menu.parentNode.className = menu.parentNode.className.replace('visible' , 'invisible')
+        menu.parentNode.className = menu.parentNode.className.replace('z-20' , 'z-0')
+    } , 500)
+}
+
+// changing root and active class to Element
+const changeRoot = e => {
+    console.log(e.target.tagName == "A");
+    if(e.target.tagName == "A"){
+        let prevLink = e.target.parentNode.parentNode.querySelector('.active')
+        prevLink.classList.remove('active')
+        e.target.parentNode.classList.add('active')
+    }
+}
+
+navElems.forEach(nav => {
+    nav.addEventListener('click' , changeRoot)
+})
+hamburger.addEventListener('click' , showMenu)
+closeModalBtn.addEventListener('click' , closeMenu)
---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/*.html'],
+  theme: {
+    screens : {
+      'xs': '480px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px'
+    },
+    container : {
+      center : true,
+      padding : {
+        DEFAULT : '1.5rem',
+        xs : '0.5rem',
+        sm : '0.5rem',
+        md : '0',
+        lg : '0',
+        xl : '0',
+      }
+    },
+    extend: {
+      colors : {
+        primary : '#D0F80C',
+        secondary : '#114246',
+        blueLight : {
+          100 : '#89DDCE',
+          200 : '#459384'
+        },
+        
+      }
+    },
+  },
+  plugins: [],
+}
+
